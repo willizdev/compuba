08edd9ba4922854e097ee239d693be58af51c79d
wz73 <williamzheng73@gmail.com>
2025-06-17 18:38:45 -0300

    guia 8 terminada

--- Algoritmos/Introducción a la Programación/Python/Guías/Guía 8 - Pilas, Colas, Diccionarios y Archivos/p8.py
+++ Algoritmos/Introducción a la Programación/Python/Guías/Guía 8 - Pilas, Colas, Diccionarios y Archivos/p8.py
@@ -305,3 +305,279 @@ def atencion_a_clientes(c: Cola[(str, int, bool, bool)]) -> Cola[(str, int, bool
         c_res.put(c_aux2.get())
 
     return c_res
+
+# ejercicio 16
+def dict_contiene(d: dict[any], k: any) -> bool:
+    for key in d.keys():
+        if k == key:
+            return True
+    return False
+
+def calcular_promedio_por_estudiante(notas: list[(str, float)]) -> dict[str, float]:
+    promedios: dict[str, float] = {}
+    contador: dict[str, int] = {}
+
+    for estudiante in notas:
+        nombre: str = estudiante[0]
+        nota: float = estudiante[1]
+
+        if not dict_contiene(contador, nombre):
+            contador[nombre] = 1
+            promedios[nombre] = nota
+            continue
+        
+        contador[nombre] += 1
+        promedios[nombre] += nota
+
+    for nombre, nota in promedios.items():
+        promedios[nombre] = nota / contador[nombre]
+
+    return promedios
+
+# ejercicio 17
+historiales: dict[str, Pila[str]] = {}
+
+def visitar_sitio(historiales: dict[str, Pila[str]], usuario: str, sitio: str) -> None:
+    if not dict_contiene(historiales, usuario):
+        historiales[usuario] = Pila()
+    historiales[usuario].put(sitio)
+
+def navegar_atras(historiales: dict[str, Pila[str]], usuario: str) -> str:
+    return historiales[usuario].get()
+
+# ejercicio 18
+def agregar_producto(inventario: dict[str, int | float], nombre: str, precio: float, cantidad: int) -> None:
+    inventario[nombre] = {
+        "precio": precio,
+        "cantidad": cantidad
+    }
+
+def actualizar_stock(inventario: dict[str, int | float], nombre: str, cantidad: int) -> None:
+    inventario[nombre]["cantidad"] = cantidad
+
+def calcular_valor_inventario(inventario: dict[str, int | float]) -> float:
+    valor: float = 0
+    for producto in inventario.values():
+        valor += producto["cantidad"] * producto["precio"]
+    return valor
+
+# ejercicio 19
+def contar_lineas(nombre_archivo: str) -> int:
+    archivo = open(nombre_archivo, "r")
+    lineas = archivo.readlines()
+    archivo.close()
+    return len(lineas)
+
+def palabras(texto: str) -> list[str]:
+    def es_palabra(caracter: chr) -> bool:
+        minusculas: str = "abcdefghijklmnopqrstuvwxyz"
+        mayusculas: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        numeros: str = "0123456789"
+        letras: str = minusculas + mayusculas + numeros + "-_"
+        for c in letras:
+            if caracter == c:
+                return True
+        return False
+
+    lista: list[str] = []
+    buff: str = ""
+
+    for c in texto:
+        if es_palabra(c):
+            buff += c
+            continue
+        if len(buff) > 0:
+            lista.append(buff)
+            buff = ""
+
+    if len(buff) > 0:
+        lista.append(buff)
+    return lista
+
+def existe_palabra(nombre_archivo: str, palabra: str) -> bool:
+    archivo = open(nombre_archivo, "r")
+    lista_palabras: list[str] = palabras(archivo.read())
+    
+    for p in lista_palabras:
+        if palabra == p:
+            return True
+
+    archivo.close()
+    return False
+
+def cantidad_de_apariciones(nombre_archivo: str, palabra: str) -> int:
+    archivo = open(nombre_archivo, "r")
+    lista_palabras: list[str] = palabras(archivo.read())
+    apariciones: int = 0
+    
+    for p in lista_palabras:
+        if palabra == p:
+            apariciones += 1
+
+    archivo.close()
+    return apariciones
+
+# ejercicio 20
+def agrupar_por_longitud(nombre_archivo: str) -> dict[int, int]:
+    archivo = open(nombre_archivo, "r")
+    lista_palabras: list[str] = palabras(archivo.read())
+    longitud: dict[int, int] = {}
+
+    for p in lista_palabras:
+        if not dict_contiene(longitud, len(p)):
+            longitud[len(p)] = 1
+            continue
+        longitud[len(p)] += 1
+
+    archivo.close()
+    return longitud
+
+# ejercicio 21
+def la_palabra_mas_frecuente(nombre_archivo: str) -> str:
+    archivo = open(nombre_archivo, "r")
+    lista_palabras: list[str] = palabras(archivo.read())
+    dict_palabras: dict[str, int] = {}
+    
+    for p in lista_palabras:
+        if not dict_contiene(dict_palabras, p):
+            dict_palabras[p] = 1
+            continue
+        dict_palabras[p] += 1
+
+    palabra: str = ""
+    frecuencia: int = 0
+
+    for p, f in dict_palabras.items():
+        if f > frecuencia:
+            palabra = p
+            frecuencia = f
+
+    archivo.close()
+    return palabra
+
+# ejercicio 22
+def clonar_sin_comentarios(nombre_archivo_entrada: str, nombre_archivo_salida: str) -> None:
+    entrada = open(nombre_archivo_entrada, "r")
+    salida = open(nombre_archivo_salida, "w")
+    lineas: list[str] = []
+
+    for linea in entrada.readlines():
+        if linea[0] == '#':
+            continue
+        lineas.append(linea)
+    
+    salida.writelines(lineas)
+    entrada.close()
+    salida.close()
+
+# ejercicio 23
+def invertir_lineas(nombre_archivo_entrada: str, nombre_archivo_salida: str) -> None:
+    entrada = open(nombre_archivo_entrada, "r")
+    salida = open(nombre_archivo_salida, "w")
+    lineas: list[str] = []
+    buff: str = ""
+
+    for caracter in entrada.read():
+        if caracter == '\n':
+            lineas.append(buff)
+            buff = ""
+            continue
+        buff += caracter
+    lineas.append(buff)
+
+    for i in range(0, int(len(lineas) / 2)):
+        temp: str = lineas[i]
+        lineas[i] = lineas[len(lineas) - 1 - i]
+        lineas[len(lineas) - 1 - i] = temp
+
+    for i in range(0, len(lineas) - 1):
+        lineas[i] += '\n'
+    
+    salida.writelines(lineas)
+    entrada.close()
+    salida.close()
+
+# ejercicio 24
+def agregar_frase_al_final(nombre_archivo: str, frase: str) -> None:
+    archivo = open(nombre_archivo, "a")
+    archivo.write('\n' + frase)
+    archivo.close()
+
+# ejercicio 25
+def agregar_frase_al_principio(nombre_archivo: str, frase: str) -> None:
+    archivo_leer = open(nombre_archivo, "r")
+    contenido: str = archivo_leer.read()
+    archivo_leer.close()
+
+    archivo_escribir = open(nombre_archivo, "w")
+    archivo_escribir.write(frase + '\n' + contenido)
+    archivo_escribir.close()
+
+# ejercicio 26
+def listar_textos_de_archivo(nombre_archivo: str) -> list[str]:
+
+    def es_legible(caracter: chr) -> bool:
+        minusculas: str = "abcdefghijklmnopqrstuvwxyz"
+        mayusculas: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        numeros: str = "0123456789"
+        letras: str = minusculas + mayusculas + numeros + " _"
+        for c in letras:
+            if caracter == c:
+                return True
+        return False
+
+    archivo = open(nombre_archivo, "rb")
+    secuencia: list[bytes] = archivo.read()
+    textos: list[str] = []
+    buff: str = ""
+
+    for c in secuencia:
+        if es_legible(chr(c)):
+            buff += chr(c)
+            continue
+        if len(buff) >= 5:
+            textos.append(buff)
+        buff = ""
+    if len(buff) >= 5:
+        textos.append(buff)
+
+    archivo.close()
+    return textos
+
+# ejercicio 27
+def calcular_promedio_por_estudiantes(nombre_archivo_notas: str, nombre_archivo_promedio: str) -> None:
+    archivo_notas = open(nombre_archivo_notas, "r")
+    archivo_promedio = open(nombre_archivo_promedio, "w")
+
+    def split_csv(texto: str) -> list[str]:
+        lista: list[str] = []
+        buff: str = ""
+        for c in texto:
+            if c == ',':
+                lista.append(buff)
+                buff = ""
+                continue
+            buff += c
+        lista.append(buff)
+        return lista
+
+    notas: dict[str, float] = {}
+    contador: dict[str, int] = {}
+    promedios: list[str] = []
+
+    for linea in archivo_notas.readlines():
+        estudiante: list[str] = split_csv(linea)
+        if not dict_contiene(notas, estudiante[0]):
+            notas[estudiante[0]] = 0
+            contador[estudiante[0]] = 0
+        notas[estudiante[0]] += float(estudiante[3])
+        contador[estudiante[0]] += 1
+
+    for k in notas.keys():
+        promedios.append(k + ',' + str(notas[k] / contador[k]))
+    for i in range(0, len(promedios) - 1):
+        promedios[i] += '\n'
+    archivo_promedio.writelines(promedios)
+
+    archivo_notas.close()
+    archivo_promedio.close()
